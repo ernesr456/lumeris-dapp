557ef4a5035abfe9319759ecf8d315eae48506a4
Fabian <Fabian@chainsquad.com>
2025-10-16 09:50:43 +0100

    Create daily gift and lottery pages

---
+++ src/pages/DailyGiftPage.tsx
@@ -0,0 +1,15 @@
+import { DailyGift } from "@/components/DailyGift";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+export default function DailyGiftPage() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1 relative z-10">
+        <DailyGift />
+      </main>
+      <Footer />
+    </div>
+  );
+}
